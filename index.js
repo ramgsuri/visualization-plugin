b1c2e50f51e4699e80636927cc46f341e7332496
Ram G Suri <suriramg@johndeere.com>
2024-04-04 16:19:43 +0530

    #CARBON-HACK [Ravinder|Ram] Adds visual-demo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const createCanvas = require('canvas').createCanvas;
-const Chart = require('chart.js');
+const fs = require('fs')
+const { createCanvas } = require("canvas");
+const { Chart, registerables } = require("chart.js");
+const csv = require('csv-parser')
 
+Chart.register(...registerables);
 
 const Visualization = (globalConfig) => {
 
@@ -11,7 +14,7 @@ const Visualization = (globalConfig) => {
 
     async function fetchDataFromCSV(csvFilePath) {
         let results = [];
-    
+
         const csvPromise = new Promise((resolve, reject) =>
             fs.createReadStream(csvFilePath)
                 .pipe(csv())
@@ -22,28 +25,28 @@ const Visualization = (globalConfig) => {
         return await csvPromise;
     }
 
-    async function createGraph(filePath) {
-        const data = await fetchDataFromCSV(filePath);
+    async function createGraph(inputFilePath, outputFilePath) {
+        const data = await fetchDataFromCSV(inputFilePath);
         console.log("DEBUG: Data", data)
-    
+
         const canvas = createCanvas(800, 600);
         const ctx = canvas.getContext('2d');
-    
+
         const legend1 = data[0].Path;
         const legend2 = data[1].Path;
-    
+
         const labels = [];
         for (let key in data[0]) {
             if (key != "Path")
                 labels.push(key)
         }
-    
+
         delete data[0].Path;
         delete data[1].Path;
-    
+
         const dataset1 = Object.values(data[0]);
         const dataset2 = Object.values(data[1]);
-    
+
         new Chart(ctx, {
             type: 'line',
             data: {
@@ -62,31 +65,25 @@ const Visualization = (globalConfig) => {
                 }]
             }
         });
-    
+
         const imageBuffer = canvas.toBuffer('image/png');
-        fs.writeFileSync('chart.png', imageBuffer);
+        fs.writeFileSync(outputFilePath, imageBuffer);
     };
 
     const execute = async (inputs, config) => {
-        const keepExisting = globalConfig['keep-existing'] === true;
-        const inputParameter = config['from-param'];
-        const outputParameter = config['to-param'];
+        const outputPath = config['output-path'];
+        console.log("Inputs", inputs)
+        console.log("Config", config)
 
         return inputs.map(input => {
-            let outputValue = input[inputParameter];
+            let inputPath = input['data-source'];
             // read the data and create Graph
+            createGraph(inputPath, outputPath);
 
-            createGraph();
-            if (input[inputParameter]) {
-                if (!keepExisting) {
-                    delete input[inputParameter];
-                }
-            }
-
-            return {
-                ...input,
-                [outputParameter]: outputValue
-            };
+            // return {
+            //     ...input,
+            //     [outputParameter]: outputValue
+            // };
         });
     };
 
