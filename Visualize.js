10498b243860db00576d1aefd6c6855f79dd9f57
Ravinder Jajoria <jajoriaravinder99@gmail.com>
2024-04-05 16:44:55 +0530

    [Ravinder] Create visaulization dynamically for all the rows in input csv

--- Visualize.js
+++ Visualize.js
@@ -1,5 +1,5 @@
-const { createCanvas } = require("canvas");
-const { Chart, registerables } = require("chart.js");
+const { createCanvas } = require('canvas');
+const { Chart, registerables } = require('chart.js');
 const csv = require('csv-parser')
 const fs = require('fs')
 
@@ -13,49 +13,45 @@ async function fetchDataFromCSV(csvFilePath) {
             .pipe(csv())
             .on('data', (data) => results.push(data))
             .on('end', () => resolve(results))
-            .on("error", (err) => reject(err))
+            .on('error', (err) => reject(err))
     );
     return await csvPromise;
 }
 
+const createDatasets = function (rows) {
+    return rows
+        .map(row => {
+            const { Path, ...data } = row;
+            return [Path, data];
+        })
+        .map(([label, rowData]) => {
+            return {
+                label: label,
+                data: Object.values(rowData),
+                borderWidth: 1
+            }
+        });
+}
+
 async function createGraph(filePath) {
     const data = await fetchDataFromCSV(filePath);
-    console.log("DEBUG: Data", data)
+    if (data.length === 0) {
+        console.error('No data found in the input source');
+        return;
+    }
+
+    const { Path: _, ...rowWithDataKeys } = data[0];
+    const labels = Object.keys(rowWithDataKeys);
+    const datasets = createDatasets(data);
 
     const canvas = createCanvas(800, 600);
     const ctx = canvas.getContext('2d');
-
-    const legend1 = data[0].Path;
-    const legend2 = data[1].Path;
-
-    const labels = [];
-    for (let key in data[0]) {
-        if (key != "Path")
-            labels.push(key)
-    }
-
-    delete data[0].Path;
-    delete data[1].Path;
-
-    const dataset1 = Object.values(data[0]);
-    const dataset2 = Object.values(data[1]);
 
     new Chart(ctx, {
         type: 'line',
         data: {
             labels: labels,
-            datasets: [{
-                label: legend1,
-                data: dataset1,
-                borderColor: 'rgb(75, 192, 192)',
-                borderWidth: 1
-            },
-            {
-                label: legend2,
-                data: dataset2,
-                borderColor: 'rgb(50, 50, 50)',
-                borderWidth: 1
-            }]
+            datasets: datasets
         }
     });
 
@@ -63,4 +59,4 @@ async function createGraph(filePath) {
     fs.writeFileSync('chart.png', imageBuffer);
 }
 
-createGraph("./computed.csv");
+createGraph('./computed.csv');
