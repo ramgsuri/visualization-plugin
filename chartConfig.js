9bbfadc8c68c6f885b72d558a14f83ad70934b45
vanshkapoor <vanshkapoorvk7@gmail.com>
2024-04-07 02:48:00 +0530

    updated the visual plugin to support html report

---
+++ chartConfig.js
@@ -0,0 +1,51 @@
+const barChartData = {
+    type: 'bar',
+    data: {
+        labels: null,
+        datasets: []
+      },
+    options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: ''
+          }
+        }
+    }
+  };
+
+const pieChartData = {
+    type: 'pie',
+    data: {
+        labels: null,
+        datasets: [{
+            data: []
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Total Carbon Emissions'
+          }
+        }
+      },
+}
+
+module.exports = {
+    barChartData,
+    pieChartData
+}
--- index.js
+++ index.js
@@ -1,90 +1,103 @@
 const fs = require('fs')
-const { createCanvas } = require("canvas");
-const { Chart, registerables } = require("chart.js");
-const csv = require('csv-parser')
+const ejs  = require('ejs')
+const path = require('path')
+const { barChartData, pieChartData } = require("./chartConfig")
+const CHART_TEMPLATE_PATH = path.resolve(__dirname, './chartTemplate.ejs');
+const REPORT_OUTPUT_PATH = './examples/outputs/report.html'
 
-Chart.register(...registerables);
+const generateHTML = (carbonBarChart, cpuEnergyBarChart, carbonPieChart, startDate, endDate, outputPath) => {
+    fs.readFile(CHART_TEMPLATE_PATH, 'utf8', (err, template) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+ 
+    const html = ejs.render(template, { carbonBarChart, cpuEnergyBarChart, carbonPieChart, startDate, endDate });
+
+    const reportOutputPath = outputPath?outputPath:REPORT_OUTPUT_PATH
+    fs.writeFile(reportOutputPath, html, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Emissions Report generated successfully.');
+    });
+    })
+}
+
+const buildCharts = (timestamp, serverCarbonUtil) => {
+    const carbonBarChart = JSON.parse(JSON.stringify(barChartData));
+    const cpuEnergyBarChart = JSON.parse(JSON.stringify(barChartData));
+    const carbonPieChart = JSON.parse(JSON.stringify(pieChartData));
+    carbonBarChart.data.labels = timestamp;
+    cpuEnergyBarChart.data.labels = timestamp;
+    carbonPieChart.data.labels = Object.keys(serverCarbonUtil);
+    carbonBarChart.options.plugins.title.text = "Carbon Emission vs time"
+    cpuEnergyBarChart.options.plugins.title.text = "CPU energy vs time"
+    
+    Object.keys(serverCarbonUtil).forEach(serverData => {
+        carbonBarChart.data.datasets.push({
+            label: serverData,
+            data: serverCarbonUtil[serverData].carbon,
+            borderWidth: 1
+        });
+        cpuEnergyBarChart.data.datasets.push({
+            label: serverData,
+            data: serverCarbonUtil[serverData]['cpu-energy'],
+            borderWidth: 1 
+        });
+        let totalCarbonValue = serverCarbonUtil[serverData].carbon.reduce((totalCarbonVal, carbonVal) => totalCarbonVal+carbonVal);
+        carbonPieChart.data.datasets[0].data.push(totalCarbonValue)
+    });
+    
+    return {
+        carbonBarChart,
+        cpuEnergyBarChart,
+        carbonPieChart
+    }
+}
+
+const generateChartMetaData = (chartCSVData) => {
+    const timestamp = [];
+    const serverCarbonUtil = {};
+
+    chartCSVData.forEach(data => {
+        if(!timestamp.includes(data.timestamp)){
+            timestamp.push(data.timestamp); // gets labels
+        }
+        if(serverCarbonUtil[data.name]) { //emissions object per server 
+            serverCarbonUtil[data.name].carbon.push(data['carbon']?data['carbon']:0);
+            serverCarbonUtil[data.name]['carbon-intensity'].push(data['grid/carbon-intensity']?data['grid/carbon-intensity']:0);
+            serverCarbonUtil[data.name]['cpu-energy'].push(data['cpu/energy']?data['cpu/energy']:0);
+        }else{
+            serverCarbonUtil[data.name] = {
+                    'carbon': [ data['carbon']?data['carbon']:0 ],
+                    'carbon-intensity': [ data['grid/carbon-intensity']?data['grid/carbon-intensity']:0 ],
+                    'cpu-energy': [ data['cpu/energy']?data['cpu/energy']:0 ]
+            };
+        }
+    })
+
+    return {
+        timestamp,
+        serverCarbonUtil
+    }
+}
 
 const Visualization = (globalConfig) => {
-
-    console.log(globalConfig)
     const metadata = {
         kind: 'execute',
     };
 
-    async function fetchDataFromCSV(csvFilePath) {
-        let results = [];
-
-        const csvPromise = new Promise((resolve, reject) =>
-            fs.createReadStream(csvFilePath)
-                .pipe(csv())
-                .on('data', (data) => results.push(data))
-                .on('end', () => resolve(results))
-                .on("error", (err) => reject(err))
-        );
-        return await csvPromise;
-    }
-
-    async function createGraph(inputFilePath, outputFilePath) {
-        const data = await fetchDataFromCSV(inputFilePath);
-        console.log("DEBUG: Data", data)
-
-        const canvas = createCanvas(800, 600);
-        const ctx = canvas.getContext('2d');
-
-        const legend1 = data[0].Path;
-        const legend2 = data[1].Path;
-
-        const labels = [];
-        for (let key in data[0]) {
-            if (key != "Path")
-                labels.push(key)
-        }
-
-        delete data[0].Path;
-        delete data[1].Path;
-
-        const dataset1 = Object.values(data[0]);
-        const dataset2 = Object.values(data[1]);
-
-        new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: legend1,
-                    data: dataset1,
-                    borderColor: 'rgb(75, 192, 192)',
-                    borderWidth: 1
-                },
-                {
-                    label: legend2,
-                    data: dataset2,
-                    borderColor: 'rgb(50, 50, 50)',
-                    borderWidth: 1
-                }]
-            }
-        });
-
-        const imageBuffer = canvas.toBuffer('image/png');
-        fs.writeFileSync(outputFilePath, imageBuffer);
-    };
-
     const execute = async (inputs, config) => {
         const outputPath = config['output-path'];
-        console.log("Inputs", inputs)
-        console.log("Config", config)
+        const { timestamp, serverCarbonUtil } = generateChartMetaData(inputs);
 
-        return inputs.map(input => {
-            let inputPath = input['data-source'];
-            // read the data and create Graph
-            createGraph(inputPath, outputPath);
+        const { carbonBarChart, cpuEnergyBarChart, carbonPieChart } = buildCharts(timestamp, serverCarbonUtil);
+    
+        generateHTML(carbonBarChart, cpuEnergyBarChart, carbonPieChart, startDate = timestamp[0], endDate = timestamp.slice(-1)[0], outputPath);
 
-            // return {
-            //     ...input,
-            //     [outputParameter]: outputValue
-            // };
-        });
+        return inputs;
     };
 
     return {
